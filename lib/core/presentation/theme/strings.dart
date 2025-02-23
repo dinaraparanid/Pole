122701bd3a1d5debc19368d67c3c2551fbef8268
dinaraparanid <dinaraparanid@gmail.com>
2025-02-23 20:20:15 +0300

    init

--- lib/core/presentation/theme/strings.dart
+++ lib/core/presentation/theme/strings.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/foundation.dart';
-// import 'package:pole/l10n/app_localizations.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pole/l10n/app_localizations.dart';
 
-// extension AppStrings on BuildContext {
-//   AppLocalizations get strings => AppLocalizations.of(this)!;
-// }
+extension AppStrings on BuildContext {
+  AppLocalizations get strings => AppLocalizations.of(this)!;
+}
